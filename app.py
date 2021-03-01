680c61c079fd03ec1cb8cab40a1eb9e13244865d
deepak725 <64675537+deepak725@users.noreply.github.com>
2021-03-01 20:17:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ classes = learn.data.classes
 
 
 def predict_single(img_file):
+
 	prediction = learn.predict(open_image(img_file))
-    probs_list = prediction[2].numpy()
+	probs_list = prediction[2].numpy()
     return {
         'category': classes[prediction[1].item()],
         'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
